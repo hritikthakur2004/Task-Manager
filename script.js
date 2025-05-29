9bd590f9ab7e01c7ea9e9b50179f96e199c5193f
Hritik Thakur <hritikthakur@gmail.com>
2025-05-29 09:12:42 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let elem = document.querySelector(".elem")
-elem.addEventListener("click",function(){
-    elem.style.color = "red"
-    console.log("hello")
+function openCards(){
+    let allelem  = document.querySelectorAll(".elem");
+let btn = document.querySelectorAll(".backbutton");
+let fullallelem = document.querySelectorAll(".fullElem")
+allelem.forEach((element) => {
+   
+    element.addEventListener("click",()=>{
+       fullallelem[element.id].style.display = 'block';
 
+    })
+    
+    
+
+});
+btn.forEach(function(back){
+    back.addEventListener("click",function(){
+         fullallelem[back.id].style.display = 'none';
+    })
 })
+
+}
+openCards();
